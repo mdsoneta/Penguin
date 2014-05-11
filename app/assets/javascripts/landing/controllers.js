138a67d85bbd0a0698a7b810b2d4f47e38e8c131
Mayur Soneta <mdsoneta80@gmail.com>
2014-05-11 10:27:40 -0500

    fixed angular and controller

--- app/assets/javascripts/landing/controllers.js
+++ app/assets/javascripts/landing/controllers.js
@@ -3,13 +3,13 @@
  */
 define(["angular"], function (angular) {
     "use strict";
-
-    var LandingCtrl = function ($scope, $location, landingService) {
-        var response = landingService.landing()
-        alert("controller repsonse >>" + response)
+    var LandingCtrl = function ($scope, landingService) {
+        landingService.landing().then(function(response) {
+            $scope.landingResults = response;
+            console.log("in controller >>" +response);
+        });
     };
-    LandingCtrl.$inject = ["$scope", "$location", "landingService"];
-
+    LandingCtrl.$inject = ["$scope", "landingService"];
     return {
         LandingCtrl: LandingCtrl
     };
--- app/assets/javascripts/landing/services.js
+++ app/assets/javascripts/landing/services.js
@@ -3,46 +3,14 @@
  */
 define(["angular", "common"], function (angular) {
     "use strict";
-
-    var mod = angular.module("landing.services", ["yourprefix.common"]);
+    var mod = angular.module("landing.services", []);
     mod.factory("landingService", ["$http", "$q", "playRoutes", function ($http, $q, playRoutes) {
-        var user, token;
         return {
-              landing: function()
-              {
-                 return playRoutes.controllers.Landing.landing().post().then(function (response)
-                  {
-                      console.log("response from services >>" + response.data.query.results.quote.Name)
-                       response.data.query.results.quote.Name
-                  });
-              }
-        };
-    }]);
-    /**
-     * Add this object to a route definition to only allow resolving the route if the user is
-     * logged in. This also adds the contents of the objects as a dependency of the controller.
-     */
-    mod.constant("userResolve", {
-        user: ["$q", "userService", function ($q, userService) {
-            var deferred = $q.defer();
-            var user = userService.getUser();
-            if (user) {
-                deferred.resolve(user);
-            } else {
-                deferred.reject();
+            landing: function () {
+                return playRoutes.controllers.Landing.landing().post().then(function (response) {
+                    return response.data.query.results.quote.Name;
+                });
             }
-            return deferred.promise;
-        }]
-    });
-    /**
-     * If the current route does not resolve, go back to the start page.
-     */
-    var handleRouteError = function ($rootScope, $location) {
-        $rootScope.$on("$routeChangeError", function (e, next, current) {
-            $location.path("/");
-        });
-    };
-    handleRouteError.$inject = ["$rootScope", "$location"];
-    mod.run(handleRouteError);
-    return mod;
+        }
+    }]);
 });
